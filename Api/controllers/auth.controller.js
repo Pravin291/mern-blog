cda755d6a9cf5913d391025482f82329deffcfbd
Pravin Bhosale <pb6884845@gmail.com>
2024-01-22 21:36:59 +0530

    create signin api route

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -11,19 +13,55 @@ export const signup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400,'All field are required'));
+    next(errorHandler(400, "All field are required"));
   }
-  const hashedpassword = bcryptjs.hashSync(password,10)
+  const hashedpassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
     email,
-    password:hashedpassword,
+    password: hashedpassword,
   });
   try {
     await newUser.save();
     res.json("signup successfully");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const {password: pass, ...rest} = validUser._doc
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+
+  } catch (error) {
+    next(error);
   }
 };
