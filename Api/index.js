cb999879b31fa814c40ecb11b21036cff8eef39b
Pravin Bhosale <pb6884845@gmail.com>
2024-01-31 17:34:49 +0530

    update

--- Api/index.js
+++ Api/index.js
@@ -22,8 +22,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.listen(3000,()=>{
-    console.log('Server is running at port 3000!');
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running at port ${process.env.PORT} `);
 });
 
 app.use('/api/user/',userRoutes)
